f0c5d655008767b4db5a6ed0714dd8493e3cfb22
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-14 23:02:07 +0100

    crearte hero file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ButtonGradient from "./assets/assets/svg/ButtonGradient";
 import { Button } from "./components/Button";
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
