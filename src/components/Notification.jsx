b83658e1726e3c912b4ba2b5fae8f989c96128ed
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-15 09:55:41 +0100

    comapny logo created

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { notificationImages } from "../constants";
+import { notification1 } from "../assets/assets";
 
 export const Notification = ({ className, title }) => {
   return (
@@ -6,6 +8,36 @@ export const Notification = ({ className, title }) => {
       className={`${
         className || ""
       } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur- border-n-1/10 rounded-2xl gap-5`}
-    ></div>
+    >
+      <img
+        src={notification1}
+        width={62}
+        height={62}
+        alt="image"
+        className="rounded-xl"
+      />
+      <div className="flex-1">
+        <h6 className="mb-1 font-semibold text-base">{title}</h6>
+        <div className="flex items-center justify-between">
+          <ul className="flex -m-0.5">
+            {notificationImages.map((item, i) => (
+              <li
+                className="flex w-6 h6 border-2 border-n-12 rounded-full overflow-hidden"
+                key={i}
+              >
+                <img
+                  src={item}
+                  className="w-full"
+                  width={20}
+                  height={20}
+                  alt={item}
+                />
+              </li>
+            ))}
+          </ul>
+          <div className="body-2 text-n-13">1m ago</div>
+        </div>
+      </div>
+    </div>
   );
 };
