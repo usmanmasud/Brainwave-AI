1a1e4cb23a190326868a0ce9d283f4d877d2d7c8
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-16 18:28:55 +0100

    /

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import Section from "./Section";
+import { smallSphere } from "../assets/assets";
 
 const Pricing = () => {
-  return <Section>Pricing</Section>;
+  return (
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            className="relative z-1"
+            width={255}
+            height={255}
+            alt="Sphere"
+          />
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Pricing;
