e0ecca4de6cc489b1ef648e5a43bcb9a97170209
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-16 23:56:21 +0100

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
