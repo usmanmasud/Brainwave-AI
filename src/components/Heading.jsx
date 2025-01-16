c1756b2879c7eb5f66102eb66c80638d17864ed4
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-16 23:12:02 +0100

    pricing .

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,9 @@ import Tagline from "./Tagline";
 
 const Heading = ({ clasName, title, text, tag }) => {
   return (
-    <div className={`${clasName} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+    <div
+      className={`${clasName} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
       {tag && <Tagline className="mb-4 md:justify-center">{tag}</Tagline>}
       {title && <h2 className="h2">{title}</h2>}
       {text && <p className="body-2 mt-4 to-n-4">{text}</p>}
